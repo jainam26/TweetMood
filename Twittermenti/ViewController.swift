cefebbf86058b4c4cc192523432ed6aae3625a08
Jainam Shah <jainam_s@icloud.com>
2019-01-15 21:58:57 -0500

    Initial Commit

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,22 +7,31 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "CYixxCxnAR0N3uB4GUqkk1r4r", consumerSecret: "FoZFYvkHIiLnCVhMBT10oO8byRAKgnsvTzTZeoPhRe9dnZRdk0")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with Twitter API request, \(error)")
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
-    
     }
+    
+    
     
 }
 
